d21e2572f816598010ad92a52f3dc0cfd9b32f5f
MikkelTheGreat <33924773+MikkelTheGreat@users.noreply.github.com>
2018-12-04 12:37:02 +0100

    Nice and neat

--- TSolution-Bordfodbold/TSolution-Bordfodbold/Abstract/IRepository.cs
+++ TSolution-Bordfodbold/TSolution-Bordfodbold/Abstract/IRepository.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace TSolution_Bordfodbold.Abstract {
-  public class IRepository {
+namespace TSolution_Bordfodbold.Abstract
+{
+    public class IRepository
+    {
 
-  }
+    }
 }
