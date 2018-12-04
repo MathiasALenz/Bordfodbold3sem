04313c80ada5e71099444a032f6df0db42a596f0
MikkelTheGreat <33924773+MikkelTheGreat@users.noreply.github.com>
2018-12-04 12:33:35 +0100

    Doinks

---
+++ TSolution-Bordfodbold/TSolution-Bordfodbold/Concrete/EFDBContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TSolution_Bordfodbold.Concrete
+{
+    public class EFDBContext : DbContext
+    {
+        public DbSet<Spiller> Spillere { get; set; }
+    }
+}
