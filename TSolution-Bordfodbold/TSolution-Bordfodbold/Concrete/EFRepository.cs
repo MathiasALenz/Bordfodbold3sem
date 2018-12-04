4118e98c661905152ffd9aff658e53966a5b1187
Mathias A. Lenz <32672754+MathiasALenz@users.noreply.github.com>
2018-12-04 12:04:25 +0100

    Setting up the Framework
    Added neccesities for MVC to work

---
+++ TSolution-Bordfodbold/TSolution-Bordfodbold/Concrete/EFRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TSolution_Bordfodbold.Abstract;
+
+namespace TSolution_Bordfodbold.Concrete {
+  public class EFRepository : IRepository{
+
+  }
+}
---
+++ TSolution-Bordfodbold/TSolution-Bordfodbold/Entities/Spiller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TSolution_Bordfodbold.Entities {
+  public class Spiller {
+    public int Spiller_ID { get; set; }
+    public string Navn { get; set; }
+    public int Scorede_Maal { get; set; }
+    public int Indkasserede_Maal { get; set; }
+    public int Vundne { get; set; }
+    public int Tabte { get; set; }
+    public int Uafgjorte { get; set; }
+    public int WS { get; set; }
+    public bool Administrator { get; set; }
+    }
+}
